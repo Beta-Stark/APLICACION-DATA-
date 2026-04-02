ddfb3e219794e05abba6b87bf5c103f006fecdd8
Beta-Stark <cateca2345@gmail.com>
2026-04-02 12:25:42 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,41 +2,22 @@ const CACHE_NAME = 'data-os-v1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Outfit:wght@300;600;900&display=swap'
+  './manifest.json'
 ];
 
-// Instalación del Service Worker y almacenamiento en caché
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
     self.skipWaiting();
 });
 
-// Activación y limpieza de versiones antiguas de caché
 self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-    return self.clients.claim();
+    event.waitUntil(self.clients.claim());
 });
 
-// Interceptor de peticiones para servir desde caché (Modo Offline)
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request).then((response) => response || fetch(event.request))
     );
 });
