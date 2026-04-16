e2c856c7b8b47c3ff49a119a8778671562a58768
Beta-Stark <cateca2345@gmail.com>
2026-04-15 21:57:42 -0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,26 @@
 importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-messaging-compat.js');
 
+// Configuración de Firebase (Versión Compat para Service Workers)
 firebase.initializeApp({
     apiKey: "AIzaSyDlmtpUfYjs33qa9tLE7Fv2C5yszSZEnUw",
+    authDomain: "data-os-db30b.firebaseapp.com",
     projectId: "data-os-db30b",
+    storageBucket: "data-os-db30b.firebasestorage.app",
     messagingSenderId: "149593183974",
     appId: "1:149593183974:web:e588fbee9a618e21980d27"
 });
 
 const messaging = firebase.messaging();
 
-// Este evento detecta cuando llega una notificación mientras la app está cerrada
+// Manejador de mensajes en segundo plano
 messaging.onBackgroundMessage((payload) => {
-    console.log('Notificación en segundo plano recibida:', payload);
+    console.log('[sw.js] Mensaje recibido en segundo plano: ', payload);
+    
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/icon.png' // Asegúrate de tener un icono o cámbialo por una URL
+        icon: '/firebase-logo.png' // Puedes cambiar esto por tu logo
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
