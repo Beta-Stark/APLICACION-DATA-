f85c30481e6c2ddb9e1d912c5274ee02c9ff2388
Beta-Stark <cateca2345@gmail.com>
2026-04-15 22:10:39 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,32 @@
 const CACHE_NAME = 'data-os-v1';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
+  '/',
+  '/index.html',
+  'https://fonts.googleapis.com/css2?family=Outfit:wght@300;600;900&display=swap'
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-    );
-    self.skipWaiting();
+// Instalación y Cache de archivos base
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
+// Estrategia: Primero Red, si falla, Caché
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
-    );
+// Escucha de Notificaciones Push
+self.addEventListener('push', (e) => {
+  const data = e.data.json();
+  const options = {
+    body: data.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968534.png',
+    badge: 'https://cdn-icons-png.flaticon.com/512/5968/5968534.png',
+    vibrate: [200, 100, 200]
+  };
+  e.waitUntil(self.registration.showNotification(data.title, options));
 });
